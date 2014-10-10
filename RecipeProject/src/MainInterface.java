7ecdd7a36bc280bbc52ce0a809b7eea6c5477044
judeanderson <judeanderson00@gmail.com>
2014-10-10 10:11:56 -0600

    Seperated the right panel again

--- RecipeProject/src/MainInterface.java
+++ RecipeProject/src/MainInterface.java
@@ -34,7 +34,7 @@ public class MainInterface extends JFrame {
 	 * Create the frame.
 	 */
 	public MainInterface() {
-		
+
 		// Need a Database object
 		final DBConnector dbObject = new DBConnector();
 		dbObject.get_DBinfo();
@@ -42,8 +42,7 @@ public class MainInterface extends JFrame {
 		final JList list_1 = new JList(dbObject.getOutput_description(0));
 		final JList list_2 = new JList(dbObject.getOutput_ingredients2(0));
 		final String[] SortItems = {"Breakfast", "Lunch", "Dinner", "Dessert"};
-		
-		
+
 		setTitle("Recipes");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 600, 600);
@@ -52,80 +51,54 @@ public class MainInterface extends JFrame {
 		contentPane.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.WHITE);
 		panel.setBorder(BorderFactory.createTitledBorder(new LineBorder(new Color(0,0,0)), "Recipes"));
 		panel.setPreferredSize(new Dimension(200, 10));
 		contentPane.add(panel, BorderLayout.WEST);
 		panel.setLayout(new BorderLayout(0, 0));
-		
+
 		ComboBox_1 dropDown = new ComboBox_1();
 		panel.add(dropDown.getComboBox(), BorderLayout.NORTH);
-	
+
 		panel.add(list);
-		
+
 		JPanel panel_2 = new JPanel();
 		panel.add(panel_2, BorderLayout.SOUTH);
 		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
+
 		JButton button = new JButton("+");
 		panel_2.add(button);
-		
+
 		JButton deleteButton = new JButton("-");
 		panel_2.add(deleteButton);
-		
-		JPanel panel_3 = new JPanel();
-		panel_3.setPreferredSize(new Dimension(400, 500));
-		panel_3.setBackground(Color.WHITE);
-		contentPane.add(panel_3, BorderLayout.EAST);
-		panel_3.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
-		final JPanel panel_1 = new JPanel();
-		panel_3.add(panel_1);
-		panel_1.setBackground(Color.WHITE);
-		panel_1.setBorder(BorderFactory.createTitledBorder(new LineBorder(new Color(0,0,0)), dbObject.getIndividualOutput_recipe(0) + " Description"));
-		panel_1.setPreferredSize(new Dimension(350, 350));
 
-		panel_1.add(list_1);
-		
-		final JPanel panel_4 = new JPanel();
-		panel_4.setBackground(Color.WHITE);
-		panel_4.setPreferredSize(new Dimension(300, 200));
-		panel_4.setBorder(BorderFactory.createTitledBorder(new LineBorder(new Color(0,0,0)), dbObject.getIndividualOutput_ingredients(0) + " Ingredients"));
-		panel_3.add(panel_4);
-		
-		panel_4.add(list_2);
-		
-		textField = new JTextField();
-		textField.setSize(400, 20);
-		panel_3.add(textField);
-		textField.setColumns(10);
-		
+		final rightPanel panel_3 = new rightPanel(dbObject.getIndividualOutput_recipe(0), dbObject.getIndividualOutput_ingredients(0), list_1, list_2);
+		contentPane.add(panel_3, BorderLayout.EAST);
+
 		list.addListSelectionListener(new ListSelectionListener() {
 			public void valueChanged(ListSelectionEvent arg0) {
 				if(arg0.getValueIsAdjusting())
 					return;
 
-		        int selectedVar;        
+		        int selectedVar;
 		        selectedVar = list.getSelectedIndex();
-		        list_1.setListData(dbObject.getOutput_description(selectedVar));    
-		        list_2.setListData(dbObject.getOutput_ingredients2(selectedVar));
-		        panel_1.setBorder(BorderFactory.createTitledBorder(new LineBorder(new Color(0,0,0)), dbObject.getIndividualOutput_recipe(selectedVar) + " Description"));
-		        panel_4.setBorder(BorderFactory.createTitledBorder(new LineBorder(new Color(0,0,0)), dbObject.getIndividualOutput_ingredients(selectedVar) + " Ingredients"));
+		        panel_3.changeRecipe(dbObject.getIndividualOutput_recipe(selectedVar), dbObject.getOutput_description(selectedVar), dbObject.getIndividualOutput_ingredients(selectedVar), dbObject.getOutput_ingredients2(selectedVar));
 			}
 		});
-	    deleteButton.addActionListener(new ActionListener() { 
-  			@Override public void actionPerformed(ActionEvent event) { 
+
+	    deleteButton.addActionListener(new ActionListener() {
+  			@Override public void actionPerformed(ActionEvent event) {
   				int selectedVar = list.getSelectedIndex();
-  				
+
   				dbObject.delete_FromDB(selectedVar);
   				Driver driver_2 = new Driver();
   				dispose();
-  			
-  			} 
+
+  			}
   		});
-		
+
 	}
 
 }
---
+++ RecipeProject/src/rightPanel.java
@@ -0,0 +1,56 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import javax.swing.event.ListSelectionEvent;
+
+public class rightPanel extends JPanel{
+
+	JList list_1;
+	JList list_2;
+	final JPanel panel_1;
+	final JPanel panel_4;
+
+	public rightPanel(String description, String ingredients, JList firstList, JList secondList) {
+
+		list_1 = firstList;
+		list_2 = secondList;
+
+		setPreferredSize(new Dimension(400, 500));
+		setBackground(Color.WHITE);
+		setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+		panel_1 = new JPanel();
+		add(panel_1);
+		panel_1.setBackground(Color.WHITE);
+		panel_1.setBorder(BorderFactory.createTitledBorder(new LineBorder(new Color(0,0,0)), description + " Description"));
+		panel_1.setPreferredSize(new Dimension(350, 350));
+
+		panel_1.add(list_1);
+
+		panel_4 = new JPanel();
+		panel_4.setBackground(Color.WHITE);
+		panel_4.setPreferredSize(new Dimension(300, 200));
+		panel_4.setBorder(BorderFactory.createTitledBorder(new LineBorder(new Color(0,0,0)), ingredients + " Ingredients"));
+		add(panel_4);
+
+		panel_4.add(list_2);
+
+		JTextField textField = new JTextField();
+		textField.setSize(400, 20);
+		add(textField);
+		textField.setColumns(10);
+
+	}
+
+	public void changeRecipe(String description_Name, Object[] description, String ingredients_Name, Object[] ingredients) {
+
+        list_1.setListData(description);
+        list_2.setListData(ingredients);
+        panel_1.setBorder(BorderFactory.createTitledBorder(new LineBorder(new Color(0,0,0)), description_Name + " Description"));
+        panel_4.setBorder(BorderFactory.createTitledBorder(new LineBorder(new Color(0,0,0)), ingredients_Name + " Ingredients"));
+	}
+}
