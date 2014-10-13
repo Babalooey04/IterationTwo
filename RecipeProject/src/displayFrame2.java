1aa31cc48c5241db8bcb888a80741506980b50f2
Babalooey04 <chelsea.carl04@gmail.com>
2014-10-13 11:18:37 -0600

    Changes to frame 2

--- RecipeProject/src/displayFrame2.java
+++ RecipeProject/src/displayFrame2.java
@@ -3,45 +3,67 @@ import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.*;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.border.LineBorder;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EtchedBorder;
 
 public class displayFrame2 extends JFrame{
-
-    final JPanel listPanel;
-    JTextField field;
     JLabel recipe_name;
-	JLabel recipe_ingredients;
 	JLabel recipe_directions;
-	JLabel listLbl;
 	JButton saveButton;
+	private JLabel lblIngredients;
+	private JLabel label;
+	private JTextArea textArea;
+	private JTextArea textArea_1;
+	private JTextArea textArea_2;
 
 	public displayFrame2(){
+		getContentPane().setPreferredSize(new Dimension(300, 300));
 
 		//settings for the frame
 		setTitle("Add Recipe");
-		setPreferredSize(new Dimension(300, 300));setSize(300,300);
+		setPreferredSize(new Dimension(600, 600));setSize(300,250);
         setBackground(Color.gray);
-
-        //creating things to go in frame
-        field = new JTextField(1000);
         recipe_name = new JLabel("Recipe name: ");
-		recipe_ingredients = new JLabel("Ingredients: ");
-		recipe_directions = new JLabel("Directions: ");
-		listLbl = new JLabel("Add");
-		saveButton = new JButton("SAVE");
-		listPanel = new JPanel();
 		String[] addOptions = {"Tsting", "One", "Two"};
-		JList options = new JList(addOptions);
-
-		//adding things to frame
-		add(field, BorderLayout.NORTH);
-        add(listPanel, BorderLayout.CENTER);
-        add(recipe_name, BorderLayout.WEST);
-        add(recipe_ingredients, BorderLayout.WEST);
-        add(recipe_directions, BorderLayout.WEST);
-        add(saveButton, BorderLayout.SOUTH);
-
-        listPanel.add(listLbl);
-		listPanel.add(options);
+		getContentPane().setLayout(new GridLayout(0, 2, 0, 0));
+        getContentPane().add(recipe_name);
+        
+        textArea = new JTextArea();
+        textArea.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        getContentPane().add(textArea);
+        
+        lblIngredients = new JLabel("Ingredients:");
+        getContentPane().add(lblIngredients);
+        
+        textArea_1 = new JTextArea();
+        textArea_1.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+        textArea_1.setWrapStyleWord(true);
+        textArea_1.setLineWrap(true);
+        getContentPane().add(textArea_1);
+        recipe_directions = new JLabel("Directions: ");
+        getContentPane().add(recipe_directions);
+        
+        textArea_2 = new JTextArea();
+        textArea_2.setWrapStyleWord(true);
+        textArea_2.setLineWrap(true);
+        textArea_2.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+        getContentPane().add(textArea_2);
+        
+        label = new JLabel("");
+        label.setBorder(null);
+        getContentPane().add(label);
+        saveButton = new JButton("SAVE");
+        saveButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	}
+        });
+        getContentPane().add(saveButton);
 
 		pack();
         setVisible(false);
