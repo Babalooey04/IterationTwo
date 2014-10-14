9ce5ecb6a5e0263267a9fe1473340054daf5ec48
Babalooey04 <chelsea.carl04@gmail.com>
2014-10-14 16:41:25 -0600

    changes to main interface

--- RecipeProject/src/MainInterface.java
+++ RecipeProject/src/MainInterface.java
@@ -31,6 +31,7 @@ import java.awt.FlowLayout;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 
 public class MainInterface extends JFrame {
@@ -49,7 +50,8 @@ public class MainInterface extends JFrame {
 		final JList list = new JList(dbObject.getOutput_recipe());
 		final JList list_1 = new JList(dbObject.getOutput_description(0));
 		final JList list_2 = new JList(dbObject.getOutput_ingredients2(0));
-		
+		final String[] SortItems = {"Breakfast", "Lunch", "Dinner", "Dessert"};
+
 		setTitle("Recipes");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 600, 600);
@@ -73,26 +75,34 @@ public class MainInterface extends JFrame {
 
 		JPanel panel_2 = new JPanel();
 		panel.add(panel_2, BorderLayout.SOUTH);
-		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		panel_2.setLayout(new GridLayout(0, 3, 0, 0));
 
 		JButton button = new JButton("+");
 		panel_2.add(button);
-
+		
 		JButton deleteButton = new JButton("-");
 		panel_2.add(deleteButton);
+				
+					    deleteButton.addActionListener(new ActionListener() {
+				  			@Override public void actionPerformed(ActionEvent event) {
+				  				int selectedVar = list.getSelectedIndex();
+				
+				  				dbObject.delete_FromDB(selectedVar);
+				  				Driver driver_2 = new Driver();
+				  				dispose();
+				
+				  			}
+				  		});
 		
-		JPanel search_panel = new JPanel();
-		search_panel.setBackground(Color.WHITE);
-		search_panel.setPreferredSize(new Dimension(200, 10));
-		search_panel.setLayout(new BorderLayout(0, 0));
+		JButton editButton = new JButton("Edit");
 		
-		JTextField search_text = new JTextField("");
-		JButton search_button = new JButton("Search");
-		
-		search_panel.add(search_text);
-		search_panel.add(search_button);
-		contentPane.add(search_panel, BorderLayout.SOUTH);
-		
+		final displayFrame2 frame_2 = new displayFrame2();
+		editButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				frame_2.showFrame();
+			}
+		});
+		panel_2.add(editButton);
 
 		final rightPanel panel_3 = new rightPanel(dbObject.getIndividualOutput_recipe(0), dbObject.getIndividualOutput_ingredients(0), list_1, list_2);
 		contentPane.add(panel_3, BorderLayout.EAST);
@@ -108,18 +118,7 @@ public class MainInterface extends JFrame {
 			}
 		});
 
-	    deleteButton.addActionListener(new ActionListener() {
-  			@Override public void actionPerformed(ActionEvent event) {
-  				int selectedVar = list.getSelectedIndex();
 
-  				dbObject.delete_FromDB(selectedVar);
-  				Driver driver_2 = new Driver();
-  				dispose();
-
-  			}
-  		});
-
-	    final displayFrame2 frame_2 = new displayFrame2();
 
 	    button.addActionListener(new ActionListener() {
   			@Override public void actionPerformed(ActionEvent event) {
@@ -127,7 +126,6 @@ public class MainInterface extends JFrame {
   				frame_2.showFrame();
   			}
   		});
-	    
 	}
 
 }
